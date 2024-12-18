268198d6c081724c00d299fe33df3c164808ca9d
dinklen08 <ryzhikh08@list.ru>
2024-12-18 19:10:38 +0300

    Delete txt temp file

--- server.go
+++ server.go
@@ -1,4 +1,4 @@
-package main
+package 
 
 import (
 	"net/http"
@@ -20,65 +20,65 @@ type InputData struct {
 	Expression string `json:"expression"`
 }
 
-func Calc(abc string) (float64, error) {
-	return 1, nil
+func ErrorOutput(w http.ResponseWriter, errText string, errCode int, errEvent error) {
+	log.Printf("[ERROR] %v", errEvent)
+	
+	json.NewEncoder(w).Encode(
+		FailureOutputData {
+			Error: errText,
+		},
+	)
+
+	w.WriteHeader(errCode)
 }
 
 func CalcHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodPost {
-		defer r.Body.Close()
+	var (
+		decryptData InputData
+		result float64
+	)
 
-		encrypt_data, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			log.Printf("[ERROR] %v", err)
-			json.NewEncoder(w).Encode( //!
-				FailureOutputData {
-					Error: "Internal server error",
-				},
-			)
+	if r.Method != http.MethodPost {
+		ErrorOutput(w, "Access denied", 405, errors.New("try to use method GET"))
+		return
+	}
 
-			w.WriteHeader(500)
-			return
-		}
+	defer r.Body.Close()
 
-		var decrypt_data InputData
-		if err = json.Unmarshal(encrypt_data, &decrypt_data); err != nil {
-			log.Printf("[ERROR] %v", err)
-			json.NewEncoder(w).Encode(
-				FailureOutputData {
-					Error: "Internal server error",
-				},
-			)
-
-			w.WriteHeader(500)
-			return
-		}
-		
-		var result float64
-		result, err = Calc(decrypt_data.Expression)
-
-		if err != nil {
-			log.Printf("[ERROR] %v", err)
-
-			json.NewEncoder(w).Encode(
-				FailureOutputData {
-					Error: "Expression is not valid",
-				},
-			)
-
-			w.WriteHeader(422)
-			return
-		}
-
-		json.NewEncoder(w).Encode(
-			SuccessOutputData {
-				Result: fmt.Sprintf("%.5f", result),
+	encrypt_data, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Printf("[ERROR] %v", err)
+		json.NewEncoder(w).Encode( //!
+			FailureOutputData {
+				Error: "Internal server error",
 			},
 		)
 
-		w.WriteHeader(200)
-		log.Printf("[INFO] success")
+		w.WriteHeader(500)
+		return
 	}
+
+	if err = json.NewDecoder(encrypt_data).Decode(&decrypt_data); err != nil {
+		ErrOutput(w, "Internal server error", 500, err)
+		return
+	}
+		
+	result, err = Calc(decrypt_data.Expression)
+
+	if err != nil {
+		ErrorOutput(w, "Expression is not valid", 422, err)
+		return
+	}
+
+	json.NewEncoder(w).Encode(
+		SuccessOutputData {
+			Result: fmt.Sprintf("%.5f", result),
+		},
+	)
+
+	w.WriteHeader(200)
+	log.Printf("[INFO] success")
+	
 }
 
 func StartServer() {
